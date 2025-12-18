56179ce690d88aa4a79c5b3870bdb905e372e535
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-12-18 00:06:04 -0300

    feat: implementa drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,22 @@
+import 'package:delivery_app_with_backend/components/my_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('H O M E  P A G E'), centerTitle: true), body: Container());
+    return Scaffold(
+      drawer: MyDrawer(),
+      appBar: AppBar(
+        title: Text(
+          'HOME PAGE',
+          style: GoogleFonts.abel(letterSpacing: 2),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
   }
 }
