7f47ac6a083c08ba46141eda42d89cc46b4a4431
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-12-18 13:12:13 -0300

    feat: implementa sliverappbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:delivery_app_with_backend/components/my_current_location.dart';
+import 'package:delivery_app_with_backend/components/my_description_box.dart';
 import 'package:delivery_app_with_backend/components/my_drawer.dart';
+import 'package:delivery_app_with_backend/components/my_sliver_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -7,16 +10,61 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String mystring = 'Welcome to the Home Page!';
+    String textCaptialized = mystring.toUpperCase();
     return Scaffold(
       drawer: MyDrawer(),
-      appBar: AppBar(
-        title: Text(
-          'HOME PAGE',
-          style: GoogleFonts.abel(letterSpacing: 2),
+      body: NestedScrollView(
+        headerSliverBuilder:
+            (context, innerBoxIsScrolled) => [
+              MySliverAppBar(
+                title: Text('Home Page'),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Divider(
+                      color:
+                          Theme.of(
+                            context,
+                          ).colorScheme.secondary,
+                      thickness: 1,
+                      indent: 25,
+                      endIndent: 25,
+                    ),
+                    MyCurrentLocation(),
+                    Divider(
+                      color:
+                          Theme.of(
+                            context,
+                          ).colorScheme.secondary,
+                      thickness: 1,
+                      indent: 25,
+                      endIndent: 25,
+                    ),
+                    MyDescriptionBox(),
+                    Divider(
+                      color:
+                          Theme.of(
+                            context,
+                          ).colorScheme.secondary,
+                      thickness: 1,
+                      indent: 25,
+                      endIndent: 25,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+        body: Container(
+          color: Colors.blue[200],
+          child: Center(
+            child: Text(
+              textCaptialized,
+              style: GoogleFonts.abel(letterSpacing: 2),
+            ),
+          ),
         ),
-        centerTitle: true,
       ),
-      body: Container(),
     );
   }
 }
