d7609fab96b4a0f92b9ecc058e26bfc8b3fb80a9
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2026-01-02 08:31:22 -0300

    feat; implementa a listagem de categorias do backend

--- lib/components/my_tab_bar.dart
+++ lib/components/my_tab_bar.dart
@@ -1,8 +1,15 @@
+import 'package:delivery_app_with_backend/models/food.dart';
 import 'package:flutter/material.dart';
 
 class MyTabBar extends StatelessWidget {
   final TabController tabController;
   const MyTabBar({super.key, required this.tabController});
+
+  List<Tab> _buildCateogoriesTabs() {
+    return FoodCategory.values.map((category) {
+      return Tab(text: category.toString().split('.').last);
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +17,10 @@ class MyTabBar extends StatelessWidget {
       color: Theme.of(context).colorScheme.secondary,
       child: TabBar(
         controller: tabController,
-        tabs: const [
-          Tab(text: 'Home'),
-          Tab(text: 'Profile'),
-          Tab(text: 'Settings'),
-        ],
+        tabs: _buildCateogoriesTabs(),
         labelColor: Theme.of(context).colorScheme.primary,
         unselectedLabelColor: Colors.grey,
+        labelStyle: const TextStyle(letterSpacing: 2),
         indicatorColor:
             Theme.of(context).colorScheme.primary,
       ),
