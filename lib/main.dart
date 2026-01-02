a10128f9c541d73f940f6e20321c14ed68ee53a9
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2026-01-02 08:30:21 -0300

    feat: add restaurant provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:delivery_app_with_backend/auth/login_or_register_page.dart';
+import 'package:delivery_app_with_backend/models/restaurant.dart';
 import 'package:delivery_app_with_backend/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(create: (context) => ThemeProvider(), child: const MyApp()));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ThemeProvider(),
+        ),
+        ChangeNotifierProvider(create: (_) => Restaurant()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +23,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
+    final themeProvider = Provider.of<ThemeProvider>(
+      context,
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
