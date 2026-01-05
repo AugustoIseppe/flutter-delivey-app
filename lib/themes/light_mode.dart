933cdc127757a2d0b1b2b47a53e2ba4c9d2b9322
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2026-01-05 10:23:02 -0300

    refactor: ajuste nas cores

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -4,8 +4,8 @@ ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.light(
     surface: Colors.grey.shade300,
     primary: Colors.grey.shade500,
-    secondary: Colors.grey.shade100, 
+    secondary: Colors.grey.shade100,
     tertiary: Colors.white,
     inversePrimary: Colors.grey.shade700,
-  )
+  ),
 );
